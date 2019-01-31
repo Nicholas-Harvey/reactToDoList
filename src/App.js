c8c964c9f2e43ce524de6e6830db322df23a1bac
Nicholas Harvey <nicholas.harvey@gmail.com>
2019-01-31 17:49:06 -0500

    todoitem.js is passsing correct titles from the state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import Todos from "./components/Todos";
 
 class App extends Component {
+  state = {
+    todos: [
+    {
+      id: 1,
+      title: "Go 'round mum's",
+      completed: false
+    },
+    {
+      id: 2,
+      title: "Get Liz back",
+      completed: false
+    },
+    {
+      id: 3,
+      title: "Sort life out",
+      completed: false
+    }
+  ]
+  }
   render() {
+    console.log(this.state.todos)
     return (
       <div className="App">
-        <h1>Best of luck to you.</h1>
+      < Todos todos={this.state.todos}/>
+      
       </div>
     );
   }
